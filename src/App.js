14c13c8fdce4ce1519e75313f06824d26496dae1
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-03-30 22:14:34 +0100

    quest 11 - rendering with props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
 import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <Contact />
+    <div>
+      <Contact name="Jeanne Foster" avatar="https://randomuser.me/api/portraits/women/90.jpg" online />
+      <Contact name="Kim Reid" avatar="https://randomuser.me/api/portraits/women/61.jpg" />
+      <Contact name="Ivan Shelton" avatar="https://randomuser.me/api/portraits/men/3.jpg" online />
     </div>
   );
 }
