d9bd789512cb63fa5108d8044ff95ea3606f725f
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-03-31 10:42:08 +0100

    contact list component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div>
-      <Contact name="Jeanne Foster" avatar="https://randomuser.me/api/portraits/women/90.jpg" online />
-      <Contact name="Kim Reid" avatar="https://randomuser.me/api/portraits/women/61.jpg" />
-      <Contact name="Ivan Shelton" avatar="https://randomuser.me/api/portraits/men/3.jpg" online />
+      <ContactList />
     </div>
   );
 }
